1e9b5f26cca69fe03a0aece6cefeca6695c57c8b
tveranjabka@gmail.com <tveranjabka@gmail.com>
2024-12-12 06:22:43 +0200

    Fixes

--- .env.cmake
+++ .env.cmake
@@ -3,11 +3,11 @@ set(VCPKG_LIB_DIR  ./vcpkg/installed/x64-windows/lib)
 set(VCPKG_BIN_DIR  ./vcpkg/installed/x64-windows/bin)
 set(BIN_DIR ./bin)
 
-# You cat set your local libraries to use in Visual Studio build
-# Vulkan SDK
-set(VULKAN_PATH  C:/VulkanSDK/1.3.236.0)
-set(GLFW_MSVC_PATH Z:/dev/deps/lib/GLFW/x64)
-set(GLFW_MSVC_INCLUDE_PATH ${GLFW_PATH}/include)
-set(GLFW_MSVC_LIB_PATH ${GLFW_PATH}/lib-vc2022)
-#glm
-set(GLM_PATH C:/Users/Mark/vcpkg/installed/x64-windows/include/glm)
+# # You cat set your local libraries to use in Visual Studio build
+# # Vulkan SDK
+# set(VULKAN_PATH  C:/VulkanSDK/1.3.236.0)
+# set(GLFW_MSVC_PATH Z:/dev/deps/lib/GLFW/x64)
+# set(GLFW_MSVC_INCLUDE_PATH ${GLFW_PATH}/include)
+# set(GLFW_MSVC_LIB_PATH ${GLFW_PATH}/lib-vc2022)
+# #glm
+# set(GLM_PATH C:/Users/Mark/vcpkg/installed/x64-windows/include/glm)
